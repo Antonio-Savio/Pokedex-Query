9eb98c8a535f9a0527c42af2bd73cc42f7b7ad4d
Antonio-Savio <antonio.savio28@gmail.com>
2024-03-23 23:56:31 -0300

    add new context

--- src/contexts/theme-context.js
+++ src/contexts/theme-context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const themes = {
     light: {
@@ -8,21 +8,40 @@ const themes = {
 
     dark: {
         color: '#D4D4D4',
-        background: '#030303'
+        background: '#030303',
+        darklessColor: '#292929'
     }
 }
 
 
 const ThemeContext = createContext({})
+const ModeContext = createContext({})
 
 const ThemeProvider = (props) => {
     const [theme, setTheme] = useState(themes.light)
+    const [isDarkMode, setDarkMode] = useState(false)
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme')
+        if (storedTheme) {
+            setTheme(JSON.parse(storedTheme))
+        }
+    }, [setTheme])
+
+    useEffect(() => {
+        const storedMode = localStorage.getItem('mode')
+        if (storedMode) {
+            setDarkMode(JSON.parse(storedMode))
+        }
+    }, [])
 
     return (
         <ThemeContext.Provider value={{ theme, setTheme }}>
-            {props.children}
+            <ModeContext.Provider value={{ isDarkMode, setDarkMode }}>
+                {props.children}
+            </ModeContext.Provider>
         </ThemeContext.Provider>
     )
 }
 
-export { ThemeContext, themes, ThemeProvider }
+export { ThemeContext, themes, ModeContext, ThemeProvider }
